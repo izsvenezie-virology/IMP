aee48f7b57b441bee0e2ce817ea1aae6396a45f1
EdoardoGiussani <egiussani@izsvenezie.it>
2025-09-16 18:02:57 +0200

    fix: parse new header format

--- src/bin/aiv_get_subtypes.py
+++ src/bin/aiv_get_subtypes.py
@@ -8,7 +8,7 @@ import sys
 from collections import defaultdict
 
 
-def select_subtypes(subtypes, prefix):
+def select_subtypes(subtypes):
     max_count = max(sum(refs.values()) for refs in subtypes.values()) if subtypes else 0
 
     detected_subtypes = []
@@ -16,11 +16,10 @@ def select_subtypes(subtypes, prefix):
     best_refs = []
     for subtype, refs in sorted(subtypes.items()):
         if sum(refs.values()) >= max_count * 0.1:
-            subtype_name = f"{prefix}{str(subtype)}"
-            detected_subtypes.append(subtype_name)
+            detected_subtypes.append(subtype)
             total_hits.append(str(sum(refs.values())))
             best_refs.append(
-                f"{subtype_name}: {max(refs, key=refs.get)} ({str(max(refs.values()))})"
+                f"{subtype}: {max(refs, key=refs.get)} ({str(max(refs.values()))})"
             )
     return detected_subtypes, total_hits, best_refs
 
@@ -33,17 +32,16 @@ sequences_na = defaultdict(lambda: defaultdict(int))
 with open(best_hits_file, "r") as f:
     for line in f:
         ref = line.split("\t", 1)[0]
-        id, subtype, seg = ref.split("|")
+        id, type, subtype, seg = ref.split("|")
         if not subtype:
             continue
-        subtype = subtype
         if seg == "HA":
             sequences_ha[subtype][ref] += 1
         elif seg == "NA":
             sequences_na[subtype][ref] += 1
 
-ha_subtypes, ha_hits, ha_refs = select_subtypes(sequences_ha, "H")
-na_subtypes, na_hits, na_refs = select_subtypes(sequences_na, "N")
+ha_subtypes, ha_hits, ha_refs = select_subtypes(sequences_ha)
+na_subtypes, na_hits, na_refs = select_subtypes(sequences_na)
 
 subtypes = "".join(ha_subtypes + na_subtypes)
 hits = ",".join(ha_hits + na_hits)
